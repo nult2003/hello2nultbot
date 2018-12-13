1a1b77f53307fae24980c4f462694854e7db5d37
Minh Nhat <nult2003@gmail.com>
2018-12-13 18:22:11 +0700

    add source to git

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=hello2nultbot-Web-Deploy.pubxml -p:Password=h4uJM21uepminXpfzyshL0pDbBNAy0mlgroLfWjuo0HGGxocBT68o7mEly0s
+
